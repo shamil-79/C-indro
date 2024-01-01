e0525c4078a05d141597da528cfdc344499b1c16
hi there its me shamil <148104756+shamil-79@users.noreply.github.com>
2024-01-01 17:54:54 +0530

    Add files via upload
    Just starting and created a console app

---
+++ ConsoleApp1/ConsoleApp1/CodeFile1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codeer
+{
+    class Code
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Console.WriteLine("enter a number to check if its prime number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//bool prime = true;
+//if (num <= 1)
+//{
+//    prime = false;
+//}
+//else
+//{
+//    for (int i = 2; i <= num / 2; i++)
+//    {
+//        if (num % i == 0)
+//        {
+//            prime = false; break;
+//        }
+
+//    }
+//}
+
+//if (prime)
+//{
+//    Console.WriteLine(num + "its a prime number");
+//}
+//else
+//{
+//    Console.WriteLine(num + "not a prime number");
+//}
